826be8f155a4237ecabf4378c5c082b391c0de83
ubuntu <stqsht@163.com>
2016-09-06 18:32:02 +0800

    添加规则说明

--- web_rec/src/main/webapp/module/welcome/ruleinfoCtrl.js
+++ web_rec/src/main/webapp/module/welcome/ruleinfoCtrl.js
@@ -1,14 +1,3 @@
 member.controller('RuleinfoCtrl', ['$scope','$rootScope','$window','$state','$filter','MemberControllerService', function($scope,$rootScope,$window,$state,$filter,MemberControllerService) {
-    /**
-     * 获取公司信息
-     */
-    $scope.init=function(){
-        var onSuccess=function (data, status) {
-            $scope.member = data.result.returnObject;
-        };
-        var onError=function (data, status) {
-        };
-        MemberControllerService.findLonginMember().then(onSuccess,onError);
-    }
-    $scope.init();
+
 }]);
